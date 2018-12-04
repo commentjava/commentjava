8bd8fc419c0580a90897ee9f99fae70bde4079c7
Loïc Carr <loic.carr@gmail.com>
2018-12-04 14:26:37 +0100

    Create test case for classes

---
+++ Test/ClassesTest.ml
@@ -0,0 +1,17 @@
+open Parser
+open Lexer
+open Main
+
+let () =
+  let file_path = "Test/files/test_classes01.java" in
+    let (file, filename) = Main.get_file file_path in
+    try
+    let input_file = open_in file in
+    let lexbuf = Lexing.from_channel input_file in
+      Location.init lexbuf file;
+      (* print_string lexbuf; *)
+      let exp = class_declaration nexttoken lexbuf in
+        print_string  exp ;
+      close_in (input_file)
+    with Sys_error s ->
+      print_endline ("Can't find file '" ^ file ^ "'")
