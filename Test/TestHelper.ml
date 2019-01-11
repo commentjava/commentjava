832077a1db65415eee73e8a4f2a4b20e23b5d4ac
Loïc Carr <loic.carr@gmail.com>
2019-01-11 21:33:14 +0100

    Show the number of skipped tests

--- Test/TestHelper.ml
+++ Test/TestHelper.ml
@@ -1,15 +1,20 @@
 module I =
   Parser.MenhirInterpreter
 
+type new_test =
+ | Normal of string
+ | Skip of string
+
 let successCount = ref 0
 let failCount = ref 0
+let skipTestsCount = ref 0
 
 (* Colors in terminal *)
 let green = "\x1b[0;32m"
 let red = "\x1b[0;31m"
 let reset_color = "\x1b[0m"
 
-let test_file assert_fct file =
+let run_assert_func assert_fct file =
   (* Run assert_fct on the file, and increment the fail counter if there is an exception, else increment the success counter *)
   try
     print_endline ("\n> Testing " ^ file);
@@ -22,6 +27,11 @@ let test_file assert_fct file =
         in print_endline ("Error : " ^ msg);
         print_endline ("Stack : " ^ stack);
         failCount := !failCount + 1; print_endline (red ^ "/!\\/!\\ " ^ file ^ " failed /!\\/!\\" ^ reset_color)
+
+let test_file assert_fct file =
+  match file with
+  | Normal f -> run_assert_func assert_fct f
+  | Skip f -> skipTestsCount := !skipTestsCount + 1
 
 let dir_is_empty dir =
   (* Return true if dir is empty except . and .. *)
@@ -44,7 +54,8 @@ let dir_contents dir =
 let rec filter_java files =
   match files with
     | [] -> []
-    | hd :: tl when Filename.check_suffix hd ".java" -> hd :: (filter_java tl)
+    | hd :: tl when Filename.check_suffix hd ".java" -> Normal hd :: (filter_java tl)
+    | hd :: tl when Filename.check_suffix hd ".java.disabled" ->  Skip hd :: (filter_java tl)
     | hd :: tl -> filter_java tl
 
 let test_dir dir assert_fct =
@@ -55,12 +66,12 @@ let test_dir dir assert_fct =
     let files = filter_java (dir_contents dir)
     in
       List.iter (test_file assert_fct) files;
-      print_endline ("\n>>> Total tests: " ^ (string_of_int (!successCount + !failCount)));
-      print_endline (">>> Passed: " ^ (string_of_int !successCount));
-      print_endline (">>> Failed: " ^ (string_of_int !failCount));
+      print_endline ("\n=== " ^ (string_of_int (!successCount + !failCount)) ^ " tests ("^ string_of_int (!skipTestsCount) ^ " Skipped) ===");
+      print_endline (">>>  Passed: " ^ (string_of_int !successCount));
+      print_endline (">>>  Failed: " ^ (string_of_int !failCount));
       match !failCount with
         | 0 -> print_endline (green ^ "SUCCESS\n" ^ reset_color);
-        | _ -> print_endline (red ^ "FAILURE\n" ^ reset_color);
+        | _ -> print_endline (red ^ "!FAILURE\n" ^ reset_color);
       exit !failCount;;
 
 let loop lexbuf result succeed fail =
